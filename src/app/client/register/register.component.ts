09db46252845db1092b54a0828f1f22ca71fde31
Marydeepa <mariadeepa29@gmail.com>
2019-04-09 11:50:48 +0530

    forgot

--- src/app/client/register/register.component.ts
+++ src/app/client/register/register.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl,NgForm } from '@angular/forms';
+import { MatStepperModule } from '@angular/material/stepper';
+import { ErrorStateMatcher } from '@angular/material/core';
+import {ClientService} from '../../service/client.service';
+import { Router } from '@angular/router';
+import { User } from '../../service/user';
+import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +13,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  form = new FormGroup({
+    
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+   
 
-  constructor() { }
+  });
+  error: { name: string, message: string } = { name: '', message: '' };
 
-  ngOnInit() {
+  constructor(public cli:ClientService,public router:Router) { }
+  register(registerForm:User)
+  {
+    this.cli.register(registerForm).catch(_error=>{
+      this.error=_error;
+      return this.error
+    })
   }
-
-}
+  resetForm(registerForm?: NgForm) {
+    if (registerForm != null)
+      registerForm.reset();
+  }
+ 
+  ngOnInit() {
+    this.resetForm();
+  }
+  
+  onSubmit() {
+    if (this.form.valid) {
+      console.log("Form Submitted!");
+      this.form.reset();
+    }
+  }
+  }
